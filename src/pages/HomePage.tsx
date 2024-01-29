b325a3b6562b396396f835e0f1c0a2eba5b81f22
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-01-29 10:01:36 +0200

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
+import DefaultMessage from 'components/DefaultMessage';
+
 const HomePage = () => (
-  <p>
-    DriveEase is your reliable partner in the world of car rental, created for
+  <DefaultMessage
+    message='DriveEase is your reliable partner in the world of car rental, created for
     maximum comfort and convenience during the vehicle rental process. Our
     application provides access to a wide selection of cars from leading
     manufacturers, providing the opportunity to rent a vehicle anywhere in the
-    city.
-  </p>
+    city.'
+  />
 );
 
 export default HomePage;
